6322ec491eb8413cfb9a9a181fe835c5dbff5a5e
Enrico Leite <enricoleite7@gmail.com>
2024-09-25 22:23:52 -0300

    Add files via upload

---
+++ exercicio 3.cpp
@@ -0,0 +1,25 @@
+
+#include <iostream>
+
+using namespace std;
+int main()
+{
+   string nomeFunc;
+   double horaTrab, valorHora, salario; 
+  
+   
+   cout<<"Digite o nome do funcionário: ";
+   cin>>nomeFunc;
+   
+   cout<<"Quantas horas o mesmo trabalhou: ";
+   cin>>horaTrab;
+   
+    valorHora = 10;
+   salario = horaTrab * valorHora;
+   
+   cout<<"este funcionario ira receber: "<<salario;
+   
+   
+   
+   
+}
